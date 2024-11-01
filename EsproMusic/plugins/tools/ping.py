870b25ddd6e3cd13b8f0afbb3159030a005c0602
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-11-01 12:44:07 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproMusic/plugins/tools/ping.py
+++ EsproMusic/plugins/tools/ping.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from pyrogram import filters
+from pyrogram import Client, filters
+import re
 from pyrogram.types import Message
 
 from EsproMusic import app
@@ -26,3 +27,16 @@ async def ping_com(client, message: Message, _):
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
     )
+def contains_link(text):
+    # Common URL patterns ke liye regex
+    url_pattern = re.compile(r"(https?://[^\s]+)|(www\.[^\s]+)|([a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+)")
+    return url_pattern.search(text) is not None
+
+# Filter for handling messages with links
+@app.on_message(filters.group)
+def delete_links(client, message):
+    if message.text:
+        if contains_link(message.text):
+            # Agar message me link ho to us message ko delete kar do
+            message.delete()
+            print(f"Deleted message with link: {message.text} \n\n join everyone @EsproSupport")
